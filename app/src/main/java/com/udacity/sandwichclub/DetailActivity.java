8da0aa80807452b4b81aff9e2bed99b63bdb332d
Miguel Osorio <ing.mosorio19@gmail.com>
2019-11-12 16:29:09 -0600

    add content view

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +16,22 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView originTV;
+    private TextView alsoKnownTV;
+    private TextView descriptionTV;
+    private TextView ingredientsTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        originTV = (TextView) findViewById(R.id.origin_tv);
+        alsoKnownTV = (TextView) findViewById(R.id.also_known_tv);
+        descriptionTV = (TextView) findViewById(R.id.description_tv);
+        ingredientsTV = (TextView) findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +54,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +67,26 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        originTV.setText(sandwich.getPlaceOfOrigin());
+
+        String alsoKnownAs = "";
+
+        for (String s : sandwich.getAlsoKnownAs())
+        {
+            alsoKnownAs += s + "\n";
+        }
+        alsoKnownTV.setText(alsoKnownAs);
+
+        String ingredients = "";
+
+        for (String s : sandwich.getIngredients())
+        {
+            ingredients += s + "\n";
+        }
+        ingredientsTV.setText(ingredients);
+
+        descriptionTV.setText(sandwich.getDescription());
 
     }
 }
